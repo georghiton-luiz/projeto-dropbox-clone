e4427ca89e496c94f9bf64687affcd7b9f45aeac
georghiton-luiz <georghiton@hotmail.com>
2021-07-11 19:53:40 -0300

    Selecionando arquivos

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -327,6 +327,8 @@ class DropBoxController{
             ${this.getFileIconView(file)}
             <div class="name text-center">${file.name}</div>
         `;
+
+        this.initEventsLi(li);
         return li;
     }
 
@@ -342,6 +344,13 @@ class DropBoxController{
 
                 this.listFileEl.appendChild(this.getFileView(data, key));
             });
+        });
+    }
+
+    initEventsLi(li){
+
+        li.addEventListener("click", e => {
+            li.classList.toggle("selected");
         });
     }
 }
